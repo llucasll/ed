7a74a9aa8ae185f4397123b9eafeb11535b72864
PedroAlvesV <pedroalvesvalentim@gmail.com>
2018-11-07 00:12:45 -0200

    Started working on LFilmes type

---
+++ LFilmes.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Filme.h"
+
+typedef struct no_filme{
+    Filme *filme;
+    struct no_filme *prox, *ant;
+}NFilme;
+
+typedef struct desc_lista{
+    int total;
+    NFilme *prim, *ult;
+}LFilmes;
+
+LFilmes * cria();
+Filme * busca(LFilmes *l, Filme *f);
+LFilmes * insere(LFilmes *l, Filme *f);
+LFilmes * retira(LFilmes *l, Filme *f);
+void imprime(LFilmes *l);
+void libera(LFilmes *l);
