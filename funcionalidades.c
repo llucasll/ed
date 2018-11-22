67e3a491cc31dd71b30948137c373b505090751c
Pedro Alves Valentim <llucasll.k@hotmail.com>
2018-11-22 16:16:11 -0200

    Update funcionalidades.c

--- funcionalidades.c
+++ funcionalidades.c
@@ -1,7 +1,23 @@
 #include "filmes.h"
 
-bool isDirectedBy(Filme f, char *diretor); //l
+#include <string.h>
+
+bool isDirectedBy(Filme f, char *diretor){
+    if(!strcmp(f.diretor,diretor))
+        return true;
+    return false;
+}
+
 ListaFilmes directedsBy(char *diretor, AB *a); //l
 
-bool isFromGenre(Filme f, char *genero); //detecta substring - l
-void removeGenre(AB *a, char *genero); //l
+bool isFromGenre(Filme f, char *genero){
+    if(!strstr(f.genero,genero))
+        return false;
+    return true;
+}
+
+void removeGenre(No *a, char *genero){
+	for(int i=0; i<a.tam; i++){
+		// TODO
+	}
+}
