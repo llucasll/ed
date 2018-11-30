398ff1ea852ff3d18db2a22675a249e49292cacc
PedroAlvesV <pedroalvesvalentim@gmail.com>
2018-11-30 01:53:40 -0200

    Implementada função add() de ab.h
    Falta testar, assim como a search()

--- ab.c
+++ ab.c
@@ -1,6 +1,77 @@
 #include "ab.h"
 
-void add(Filme f);
+void add(Filme f){
+    No* divisao(No* no1, int i, No* no2){
+        No* no3 = criaNo();
+        no3->tam = t-1;
+        no3->ehFolha = no2->ehFolha;
+        int j;
+        for(j=0; j<t-1; j++) no3->filmes[j] = no2->filmes[j+t];
+        if(!no2->ehFolha){
+            No* filhos_no2 = getFilhos(no2);
+            No* filhos_no3 = getFilhos(no3);
+            for(j=0; j<t; j++){
+                filhos_no3[j] = filhos_no2[j+t];
+                filhos_no2[j+t] = NULL;
+            }
+            updateFilhos(filhos_no2, no2);
+            updateFilhos(filhos_no3, no3);
+        }
+        no2->tam = t-1;
+        No* filhos_no1 = getFilhos(no1);
+        for(j=no1->tam; j>=i; j--) filhos_no1[j+1] = filhos_no1[j];
+        filhos_no1[i] = no3;
+        updateFilhos(filhos_no1, no1);
+        for(j=no1->tam; j>=i; j--) no1->filmes[j+1]=no1->filmes[j];
+        no1->filmes[i-1] = no2->filmes[t-1];
+        no1->tam++;
+        return no1;
+    }
+    No* insere_incompleto(No* no, Filme f){
+        int i = no->tam-1;
+        if(no->ehFolha){
+            while((i>=0) && (ehMenor(f, no->filmes[i]))){
+                no->filmes[i+1] = no->filmes[i];
+                i--;
+            }
+            no->filmes[i+1] = f;
+            no->tam++;
+            return no;
+        }
+        while((i>=0) && (ehMenor(f, no->filmes[i]))) i--;
+        i++;
+        if(getFilho(no, i)->tam == ((2*t)-1)) {// getFilho recebe ponteiro, passei variável
+            no = divisao(no, i+1, getFilho(no, i));
+            if(ehMenor(no->filmes[i], f)) i++;
+        }
+        No* filhos = getFilhos(no);
+        filhos[i] = insere_incompleto(getFilho(no, i), f);
+        updateFilhos(filhos, no);
+        return no;
+    }
+
+    No no = getRaiz();
+    if(search(no, f)) return no;
+    if(!no){
+        no = criaNo();
+        no->filmes[0] = f;
+        no->tam = 1;
+        return no;
+    }
+    if(no->tam == (2*t)-1){
+        No* no_s = criaNo();
+        no_s->tam = 0;
+        no_s->ehFolha = false;
+        No* filhos = getFilhos(no_s);
+        filhos[0] = no;
+        updateFilhos(filhos, no_s);
+        no_s = divisao(no_s,1,no);
+        no_s = insere_incompleto(no_s,f);
+        return no_s;
+    }
+    no = insere_incompleto(no,f);
+    return no;
+}
 
 void rm(char* titulo, int ano);
 
@@ -15,7 +86,7 @@ Filme search(No no, char* titulo, int ano){
         filmes = no.filmes;
         if(!ehMenor(filmes[i], ref)) break;
     }
-    if(i < no.tam && comparaFilmes(&filmes[i], &ref)) return filmes[i];
+    if(i < no.tam && comparaFilmes(&filmes[i], &ref)) return filmes[i]; // TODO verificar
     if(no.ehFolha) return NULL;
     return search(getFilho(no, i), titulo, ano);
 }
