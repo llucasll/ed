8d9810994dfaa9b8b9dafc27ce83840490e5bfa8
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2018-12-08 02:45:44 -0200

    Conserto de funcionalidades.c
    Não pude testar por erros de dependência ("undefined reference to 'getRaiz'" e afins)

--- application/funcionalidades.c
+++ application/funcionalidades.c
@@ -7,24 +7,24 @@ bool isDirectedBy(Filme f, char *diretor){
         return true;
     return false;
 }
-/*
-ListaFilmes* directedsBy(char *diretor, No a){
+
+ListaFilmes* directedsBy(char* diretor, No a){
     ListaFilmes* recursive_insertion(ListaFilmes* l, No a){
+        if(!a.tam) return l;
         int i;
         for(i=0; i<a.tam; i++){
-            if (isDirectedBy(a.filmes[i], diretor)){
+            l = recursive_insertion(l, getFilho(a, i));
+            if(isDirectedBy(a.filmes[i], diretor))
                 l = insere(l, a.filmes[i]);
-            }
-            if(!a.ehFolha) l = recursive_insertion(l, getFilho(a, i));
         }
-        if(!a.ehFolha) l = recursive_insertion(l, getFilho(a, i));
+        l = recursive_insertion(l, getFilho(a, i));
         return l;
     }
-    ListaFilmes * lfilmes = cria();
+    ListaFilmes* lfilmes = cria();
     lfilmes = recursive_insertion(lfilmes, a);
     return lfilmes;
 }
-*/
+
 bool isFromGenre(Filme f, char *genero){
     if(!strstr(f.genero,genero))
         return false;
@@ -33,14 +33,11 @@ bool isFromGenre(Filme f, char *genero){
 
 void removeGenre(No a, char* genero){
     if(!a.tam) return;
-    int i=0, qtd_filmes_retirados=0;
+    int i;
     for(i=0 ; i < a.tam ; i++){
-        if (isFromGenre(a.filmes[i], genero)){
-            rm(a, a.filmes[i].titulo, a.filmes[i].genero, t);
-            qtd_filmes_retirados++;
-        }
-        removeGenre(getFilho(a, i),genero);
+        removeGenre(getFilho(a, i), genero);  
+        if(isFromGenre(a.filmes[i], genero))
+            rm(a.filmes[i].titulo, a.filmes[i].ano);
     }
     removeGenre(getFilho(a, i), genero);
-    a.tam -= qtd_filmes_retirados;
 }
